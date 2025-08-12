c24e72dd19123e4da6fb4437a345aa7abc92c979
Chris Arnold <Chris.Arnold@nmtafe.wa.edu.au>
2025-08-12 10:50:28 +0800

    session 4 exercises/demos

---
+++ Session4/Exercises/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = { "one", "two", "three" };
+            string[] filtered=Array.FindAll(numbers,
+                x=>x.EndsWith('e'));
+
+            foreach (string s in filtered)
+            {
+                Console.WriteLine(s);
+            }
+        }
+        static int subtract(int x, int y) {
+            return x - y;
+        }
+        static int add(int x, int y) {
+            return x + y;
+        }
+        static void WelcomeMessage(string name="Fool!")
+        {
+            Console.WriteLine("-----------------");
+            Console.WriteLine($"Hi, {name}!");
+            Console.WriteLine("-----------------");
+        }
+    }
+}
---
+++ Session4/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+int getArea(int width,int height)
+{
+    int area = width * height;
+    return area;
+}
+int wiiiiiidth = 10;
+int heiggggght = 80;
+int area = getArea(wiiiiiidth, heiggggght);
+Console.WriteLine(area);
+Console.WriteLine(getArea(94,183));
+
+printThings(1);
+
+void printThings(int thing1=1, string thing2="Thing 2")
+{
+    Console.WriteLine("Thing " + thing1);
+    if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+    {
+        return;
+    }
+    Console.WriteLine(thing2);
+}
+
+int add(int thing1, int thing2)
+{
+    return 0;
+}
