cb355071201312a281bb8b4265f709cdf9486d7a
Chris Arnold <Chris.Arnold@nmtafe.wa.edu.au>
2025-09-09 11:36:31 +0800

    add session 8 for Tuesday

---
+++ Session 8/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Session_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact person1 = new Contact();
+            person1.Name = "Kal-El";
+            person1.Address = "30 Aberdeen St";
+            person1.Email = "kal-el@email.com";
+            Contact person2 = new Contact()
+            {
+                Name = "Chris",
+                Address = "30 Aberdeen St",
+                Email = "chris.arnold@nmtafe.wa.edu.au"
+            };
+            ContactCSVStorage csvWriter = new ContactCSVStorage("..\\..\\..\\data.csv");
+            //csvWriter.SaveContacts(new Contact[] { person1, person2 });
+
+            Contact[] fromFile=csvWriter.ReadContacts();
+            foreach (Contact contact in fromFile)
+            {
+                Console.WriteLine(contact.Name);
+            }
+        }
+
+
+
+        void DoGeometryThings()
+        {
+            {
+                Rectangle rectangle = new Rectangle(3, 7);
+                Rectangle rectangle2 = new Rectangle(4, 10);
+                Console.WriteLine(rectangle.GetArea());
+                Console.WriteLine(rectangle2.GetArea());
+                Square square1 = new Square(7);
+                Console.WriteLine(square1.GetArea());
+            }
+        }
+    }
+}
---
+++ Session 8/Square.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_8
+{
+    public class Rectangle
+    {
+        public Rectangle(int width,int height) {
+            this.Width = width;
+            this.Height = height;
+        }
+        public int Height { get;private set; }
+        public int Width { get; private set; }
+        public int GetArea()
+        {
+            return Height * Width;
+        }
+    }
+    internal class Square : Rectangle {
+        public Square(int length) : base(length,length) { }
+    }
+}
