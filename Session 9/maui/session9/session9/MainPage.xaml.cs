51ea3822d67d2ea5b62b0027d8bdc2dcff587849
Chris Arnold <mysteryproducer@gmail.com>
2025-09-19 23:08:53 +0800

    MAUI example for s9

---
+++ Session 9/maui/session9/session9/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace session9
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void textEntered(object sender, EventArgs e)
+        {
+            //This is invoked when you finish typing into the "sampleInput" text box.
+            //The MAUI label is slightly different from the WPF one. Its text property
+            //is .Text rather than .Content. More consistent than WPF!
+            nameLabel.Text = sampleInput.Text;
+        }
+
+        void tinyBurger_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            //This is invoked when the slider moves
+            //The slider is raising the event, so it's the "sender"
+            Slider slider = (Slider)sender;
+            int burgerCount = (int)slider.Value;
+            burgerText.Text = "🍔".Repeat(burgerCount);
+        }
+    }
+}
+
---
+++ Session 9/maui/session9/session9/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace session9
+{
+	public static class Utils
+	{
+		public static string Repeat(this string toRepeat,int count)
+		{
+			string result = "";
+			for (int i=0;i<count;++i)
+			{
+				result += toRepeat;
+			}
+			return result;
+		}
+	}
+}
+
