d38e3fdb8b3ded8ca84a73ea02ac33f37e2f1ff6
Chris Arnold <chris.arnold@nmtafe.wa.edu.au>
2025-09-23 16:10:07 +0800

    Ported session 10 exercise to MAUI

---
+++ Session 10/maui/ListExercise/ListExercise/ListExercise/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListExercise
+{
+    public partial class MainPage : ContentPage
+    {
+        private DataService service = new DataService();
+        public MainPage()
+        {
+            InitializeComponent();
+            enrolmentPicker.ItemsSource = Enum.GetValues(typeof(EnrolmentStatus));
+        }
+
+        /// <summary>
+        /// This event is triggered when the user selects a student from the list.
+        /// TODO: Update the controls to show the properties of the selected student.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void studentList_ItemSelected(System.Object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// This event triggers when the combo box selection is changed.
+        /// TODO: It should update the selected students enrolment.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void enrolmentPicker_SelectedIndexChanged(System.Object sender, System.EventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// This is called when the get students button is pressed.
+        /// TODO: Update this button to load the students from the service and update the list.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void getStudents_Clicked(System.Object sender, System.EventArgs e)
+        {
+        }
+    }
+}
+
