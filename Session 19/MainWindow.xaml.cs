1c5e4d0ae86e65479dc31624e7bb45f856625c7c
Chris Arnold <chris.arnold@nmtafe.wa.edu.au>
2025-12-04 18:14:07 +0800

    File selecta

---
+++ Session 19/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace Session_19;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e) {
+        OpenFolderDialog folDiaglogue = new OpenFolderDialog();
+        bool? allIsOk = folDiaglogue.ShowDialog();
+        string shozenPath = null;
+        if (allIsOk == true) {
+            shozenPath = folDiaglogue.FolderName;
+            MessageBox.Show(shozenPath);
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+        bool? result = ofd.ShowDialog();
+        if (result == true) {
+            pathBox.Text = ofd.FileName;
+        }
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e) {
+        MessageBox.Show(pathBox.Text);
+    }
+}
